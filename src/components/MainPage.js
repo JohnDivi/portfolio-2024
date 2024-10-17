c8419391ae22e53b33f8772c230dac32fae927f9
JohnDivi <johnbdivinagracia23@gmail.com>
2024-10-16 19:21:10 -0600

    Modify the structure of project and remove unneccessary files.

---
+++ src/components/MainPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MainPage() {
+    return (
+        // Header
+        // Name:    John Divinagracia
+        // Tagline: Aspiring Software Developer
+        // Nav:
+        //      - About Me
+        //      - Projects
+        //      - Skills
+        //      - Contact
+        //      - Linkedin, GitHub
+
+        // Body
+        // Footer
+        <p>This is a test</p>
+    );
+}
+
+export default MainPage;
