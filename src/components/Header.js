d40772ce99220cbdcd35b090a87354b68f09be20
JohnDivi <johnbdivinagracia23@gmail.com>
2024-10-16 20:29:50 -0600

    Add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,32 @@ import React from "react";
 
 function Header() {
     return (
-        <p>This is my Header</p>
+        // Header
+        // Name:    John Divinagracia
+        // Tagline: Aspiring Software Developer
+        // Nav:
+        //      - About Me
+        //      - Projects
+        //      - Skills
+        //      - Contact
+        //      - Linkedin, GitHub
+        <header>
+            <h1>John Divinagracia, CS Student</h1>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Projects</li>
+                </ul>
+                <section>
+                    {/* LinkedIn Image */}
+                    <img src="src/media/linkedin-icon.png"/>
+
+                    {/* GitHub Image */}
+                    <img src=""/>
+
+                </section>
+            </nav>
+        </header>
     );
 }
 
